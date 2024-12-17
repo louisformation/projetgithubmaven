cff5be246a46873a0ba9154bfe1fd48284fb5f97
Nzogne louis <louisnzogne3@gmail.com>
2024-12-17 14:37:22 +0100

    first commit

---
+++ src/main/java/com/gretadevops/calculatord/CalculatordApplication.java
@@ -0,0 +1,13 @@
+package com.gretadevops.calculatord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatordApplication.class, args);
+	}
+
+}
